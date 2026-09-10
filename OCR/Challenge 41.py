fb0d58fe5b2079fb414805d860dc307df31cc9e0
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2026-09-10 08:40:37 +0100

    Create Challenge 41.py

---
+++ OCR/Challenge 41.py
@@ -0,0 +1,28 @@
+# Challenge 41 - Prime Factorisation
+# Created: 10/09/2026
+# Last Updated: 10/09/2026
+
+def prime_factor(n):
+    i = 2
+    factors = []
+    while n > 1:
+      if n % i == 0:
+          factors.append(i)
+          n = n // i
+      else:
+          i += 1
+  
+    return factors
+    
+def main():
+    while True:
+      try:
+        x = input("Enter a number (prime factorisation): ").strip().lower()
+        if x in ("q", "quit", "exit"):
+          break
+        x = int(x)
+        print(prime_factor(x))
+      except ValueError:
+        print("You must enter a positive number.")
+  
+main()
