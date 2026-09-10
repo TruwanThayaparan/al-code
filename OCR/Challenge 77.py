5afde5381c5192b09c9384ffea8fbbedf9c5ef63
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2026-09-10 14:07:09 +0100

    Create Challenge 77.py

---
+++ OCR/Challenge 77.py
@@ -0,0 +1,28 @@
+# Challenge 77 - Fib on a chi
+# Created: 10/09/2026
+# Last Updated: 10/09/2026
+
+def fibonacci_loop(n):
+    fib = []
+    a, b = 0, 1
+    for i in range(n):
+        a, b = b, a + b
+        # e = a; a = b; b = e + b
+        # print(a)
+        fib.append(a)
+
+    return fib
+    
+def fl_until_4d():
+    fib = []
+    a, b = 0, 1
+    while True:
+        a, b = b, a + b
+        fib.append(str(a))
+        if len(str(a)) == 1000:
+            x = fib.index(str(a))
+            return a, x
+
+fib4, x = fl_until_4d()
+print(fib4)
+print(x + 1)
