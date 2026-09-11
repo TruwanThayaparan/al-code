292717773b160f44eca6251e6ccbdb0d93124cd8
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2026-09-11 13:41:27 +0100

    Update Challenge 25.py

--- OCR/Challenge 25.py
+++ OCR/Challenge 25.py
@@ -2,6 +2,33 @@
 # created: 11/09/2026
 # last updated: 11/09/2026
 
-nums = []
-print(sorted(nums))
-print(list(reversed(sorted(nums))))
+def sort(nums):
+    while True:
+        typ = input("Order by ascension or descension: ").strip().lower()
+        if typ not in ("ascend", "ascending", "ascension", "asc") and typ not in ("descend", "descending", "descension", "desc"):
+            print("Invalid.")
+            continue
+        break
+
+    if typ in ("ascend", "ascending", "ascension", "asc"):
+        print(sorted(nums))
+    else:
+        print(list(reversed(sorted(nums))))
+
+def main():
+    while True:
+        try:
+            txt = input("Enter 10 numbers: ").strip().lower()
+            if txt in ("quit", "q", "exit"):
+                break
+            txtsp = txt.split()
+            if len(txtsp) != 10:
+                print("You must enter 10 integers.")
+                continue
+            for i in range(len(txtsp)):
+                txtsp[i] = int(txtsp[i])
+            sort(txtsp)
+        except ValueError:
+            print("You must enter 10 integers.")
+
+main()
