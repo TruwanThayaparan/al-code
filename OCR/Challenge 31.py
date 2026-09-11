6481430ddfaba6a4d3eceb62add0afd0e68d81af
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2026-09-11 14:13:25 +0100

    Create Challenge 31.py

---
+++ OCR/Challenge 31.py
@@ -0,0 +1,18 @@
+# challenge 31 - Forwards and Backwards
+# created: 11/09/2026
+# last updated: 11/09/2026
+
+def main():
+    print("This is a palindrome checker.")
+    while True:
+        text = input("Enter something ('q' to quit): ")
+        if text in ('q', 'quit', 'exit'):
+            break
+        text_rev = "".join(reversed(text))
+        print(text_rev)
+        if text == text_rev:
+            print("This is a palindrome!\n")
+        else:
+            print("This is not a palindrome.\n")
+
+main()
