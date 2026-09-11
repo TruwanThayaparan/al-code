4070c9aad4cb184f824ddd7a535c9726c7576a33
Truwan Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2026-09-11 18:30:45 +0100

    Simplify text reversal in palindrome checker
    Refactor palindrome checker to use slicing for reversal.

--- OCR/Challenge 31.py
+++ OCR/Challenge 31.py
@@ -1,6 +1,6 @@
-# challenge 31 - Forwards and Backwards
-# created: 11/09/2026
-# last updated: 11/09/2026
+# Challenge 31 - Forwards and Backwards
+# Created: 11/09/2026
+# Last updated: 11/09/2026
 
 def main():
     print("This is a palindrome checker.")
@@ -8,7 +8,8 @@ def main():
         text = input("Enter something ('q' to quit): ")
         if text in ('q', 'quit', 'exit'):
             break
-        text_rev = "".join(reversed(text))
+        text_rev = text[::-1]
+        #text_rev = "".join(reversed(text))
         print(text_rev)
         if text == text_rev:
             print("This is a palindrome!\n")
