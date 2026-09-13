3c08cd69b5c1f87470021955541fb4ece2741d77
T Thayaparan <21thayaparan@parmiters.herts.sch.uk>
2026-09-13 21:14:34 +0100

    Fix print statements and credit loss messages

--- OCR/Challenge 05.py
+++ OCR/Challenge 05.py
@@ -30,20 +30,27 @@ def main():
         if r1 == r2 == r3:
             if r1 == "Skull":
                 credit = 0
-            elif r2 == "Bell":
+                print("You rolled 3 Skulls - you lost all your credit!")
+            elif r1 == "Bell":
                 credit += 500
+                print("You rolled 3 Bells - you earned 500 credit!")
             else:
                 credit += 100
+                plural = "Cherries" if r1 == "Cherry" else f"{r1}s"
+                print(f"You rolled 3 {plural} - you earned 100 credit!")
+                
         elif r1 == r2 or r2 == r3 or r1 == r3:
             if random_three.count("Skull") == 2:
                 credit = max(0, credit - 100)
+                print("You rolled 2 Skulls - you lost 100 credit!")
             else:
                 credit += 50
-                    
+                print("You rolled 2 of the same object - you earned 50 credit!")
+
         if credit < 20:
             print()
             print(f"Credit: £{credit/100:.2f}")
-            print("You lose! Not enough credit left to spin.")
+            print("You lose! Not enough credit left to spin again.")
             break
 
         print()
